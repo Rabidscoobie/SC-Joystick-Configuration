6dabecb80662368b1fd81c070627c25061d5c776
Dan <danricho@hotmail.com>
2016-07-13 08:52:42 +1000

    Debugging in console

--- public/js/main.js
+++ public/js/main.js
@@ -39,8 +39,10 @@ $( document ).ready(function() {
       if ($(this).hasAttr("title")){ // if it has a 'title' attribute
       	if (!$(this).attr("title").includes("Download")) { // if not a download link
           $(this).attr('target','_blank')
-          ga('send', 'event', 'outbound', 'click', $(this).attr("title"));
-          console.log("Sending GA Event: " + {"type": "outbound", "trigger": "click", "description": $(this).attr("title")});
+          $(this).click(function(){
+            ga('send', 'event', 'outbound', 'click', $(this).attr("title"));
+            console.log("Sending GA Event: " + {"type": "outbound", "trigger": "click", "description": $(this).attr("title")});
+          });
         } 
         else { // it is a download link
           $(this).click(function(){
@@ -51,8 +53,10 @@ $( document ).ready(function() {
       }
       else { // if it doesn't have a 'title' attribute
         $(this).attr('target','_blank')
-        ga('send', 'event', 'outbound', 'click', $(this).attr("href"));
-        console.log("Sending GA Event: " + {"type": "outbound", "trigger": "click", "description": $(this).attr("href")});
+        $(this).click(function(){
+          ga('send', 'event', 'outbound', 'click', $(this).attr("href"));
+          console.log("Sending GA Event: " + {"type": "outbound", "trigger": "click", "description": $(this).attr("href")});
+        });
       }
     }
   });
